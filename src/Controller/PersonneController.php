9fe870f9b39a5c8b5545ee899459479eb0dea44f
HoussemChourou <houssam.chourou@insat.usar.tn>
2025-05-14 20:36:20 +0100

    manipulation tableau Personne avec DOCTRINE

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/personne')]
 final class PersonneController extends AbstractController
 {
-    #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    #[Route('/', name: 'personne.list')]
+    public function index(ManagerRegistry $doctrine): Response{
+        $entityManager = $doctrine->getRepository(Personne::class);
+        $personnes = $entityManager->findAll();
+        return $this->render('personne/index.html.twig',
+            ['personnes' => $personnes]);
+    }
+
+    #[Route('/{id<\d+>}', name: 'personne.detail')]
+    public function detail(ManagerRegistry $doctrine,Personne $personne = null): Response{
+        if($personne){
+            return $this->render('personne/detail.html.twig',
+                ['personne' => $personne]);
+        }
+        else{
+            $this->addFlash('error',"Personne n'existe pas");
+            return $this->redirectToRoute('personne.list');
+        }
+
+    }
+
+    #[Route('/add', name: 'personne.add')]
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
+        $entityManager = $doctrine->getManager();
+        $personne = new Personne();
+        $personne->setFirstname('Houssem');
+        $personne->setName('Chourou');
+        $personne->setAge(21);
+
+
+        $entityManager->persist($personne);
+        $entityManager->flush();
+        return $this->render('personne/detail.html.twig', [
+            'personne' => $personne,
         ]);
+    }
+
+    #[Route('/alls/{page?1}/{nbre?12}', name: 'personne.list.alls')]
+    public function indexAlls(ManagerRegistry $doctrine,$page,$nbre): Response{
+        $entityManager = $doctrine->getRepository(Personne::class);
+        $personnes = $entityManager->findBy([], [] ,$nbre,($page-1)*$nbre);
+        return $this->render('personne/index.html.twig',
+            ['personnes' => $personnes]);
     }
 }
